121b7864659ee7ddf60131be6e1953263e907c5f
Carelle Ivana <ecdjofan01@monccnb.ca>
2023-12-27 21:12:38 -0400

    Ajoutez des fichiers projet.

---
+++ HelloGitHubForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace github
+{
+    public partial class HelloGitHubForm : Form
+    {
+        public HelloGitHubForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello GitHub.");
+        }
+    }
+}
